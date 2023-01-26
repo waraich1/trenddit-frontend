3c7d35cdc7fc3a422ab236a352f9a161eadef0d6
waraich1 <waraich@mcmaster.ca>
2023-01-26 15:54:54 -0500

    Now you can select the keyword by accessing the local state

--- src/Components/DropdownMenu/DropdownMenu.js
+++ src/Components/DropdownMenu/DropdownMenu.js
@@ -1,25 +1,27 @@
-import { Dropdown } from 'semantic-ui-react'
+import { Dropdown } from "semantic-ui-react";
 import React, { useState } from "react";
-import 'semantic-ui-css/semantic.min.css';
-
-
+import "semantic-ui-css/semantic.min.css";
 
 function DropDownMenu(input, title) {
+  var [currentValue, setCurrentValue] = useState("");
+  var options = [];
+  const handleChange = (e, { value }) => {
+    setCurrentValue(value);
+    console.log(value);
+  };
 
-    
-    var [currentValue, setCurrentValue] = useState("");
-    var options = [];
-
-    input.map((item) => options.push({key: item, value: item, text: item}))
-
-
-    return ( <Dropdown placeholder={title} selection options={options} value = {currentValue} onChange={setCurrentValue}/>
-    );
-  }
-
-
+  input.map((item) => options.push({ key: item, value: item, text: item }));
+  // console.log("This" + JSON.stringify(currentValue));
+  console.log("This is " + currentValue);
+  return (
+    <Dropdown
+      placeholder={title}
+      selection
+      options={options}
+      value={currentValue}
+      onChange={handleChange}
+    />
+  );
+}
 
 export default DropDownMenu;
-
-
-
--- src/Pages/Trends/Trends.js
+++ src/Pages/Trends/Trends.js
@@ -2,39 +2,42 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getTrendsData } from "./TrendsSlice";
 import TrendsLineGraph from "../../Components/Graphs/TrendsGraph/TrendsLineGraph";
-import dropDownMenu from "../../Components/DropdownMenu/DropdownMenu"
-
-
+import dropDownMenu from "../../Components/DropdownMenu/DropdownMenu";
 
 function Trends() {
-    const dataSubreddit = [];
-    const currentlySelectedTrend = "";
-    const dataTrend = [];
-    const [trendWords, setTrendWords] = useState([]);
-    const [subredditNames, setSubredditNames] = useState([]);
-    const dispatch = useDispatch();
-    const [subreddit, setSubreddit] = useState("");
-    const [trendKeyword, setTrendKeyword] = useState("");
-    const [error, setError] = useState(null);
+  const dataSubreddit = [];
+  const currentlySelectedTrend = "";
+  const dataTrend = [];
+  const [trendWords, setTrendWords] = useState([]);
+  const [subredditNames, setSubredditNames] = useState([]);
+  const dispatch = useDispatch();
+  const [subreddit, setSubreddit] = useState("");
+  const [trendKeyword, setTrendKeyword] = useState("");
+  const [error, setError] = useState(null);
 
-    
+  const trends = useSelector((state) => state.trends);
 
-    const trends = useSelector((state) => state.trends);
+  const onUpdateTrendWords = async (event) => {
+    event.preventDefault();
+    const new_set = new Set(trendWords);
+    new_set.add(trendKeyword);
+    const new_arr = Array.from(new_set);
 
-    const onUpdateTrendWords = async (event) => {
-        event.preventDefault();
-        setTrendWords( arr => [...arr, trendKeyword]);
-        setTrendKeyword("")
-    };
-    const onUpdateSubredditNames = async (event) => {
-        event.preventDefault()
-        setSubredditNames( arr => [...arr, subreddit]);
-        setSubreddit("")
-    };
-    
-    const handleClickForTrends = async (event) => {
-        console.log("HERE IS THE TRENDS MENU SELECTION")
-        console.log(trendsMenu)
+    setTrendWords(new_arr);
+    setTrendKeyword("");
+  };
+  const onUpdateSubredditNames = async (event) => {
+    event.preventDefault();
+    const new_set = new Set(subredditNames);
+    new_set.add(subreddit);
+    const new_arr = Array.from(new_set);
+    setSubredditNames(new_arr);
+    setSubreddit("");
+  };
+
+  const handleClickForTrends = async (event) => {
+    console.log("HERE IS THE TRENDS MENU SELECTION");
+    console.log(trendsMenu);
     event.preventDefault();
     dispatch(getTrendsData());
   };
@@ -52,102 +55,94 @@ function Trends() {
   if (trends.status === "succeeded") {
     console.log("This succeeded");
     const data = trends.data;
-    console.log(data)
-    for (var [key,value] of Object.entries(data.data)) {
-        dataTrend.push({
-            query: value.query,
-            date: value.date
-        });
+    console.log(data);
+    for (var [key, value] of Object.entries(data.data)) {
+      dataTrend.push({
+        query: value.query,
+        date: value.date,
+      });
     }
-    for (let i = 0; i < trendWords.length; i++){
-        let graphData = []
-        for (var [key,value] of Object.entries(data.data)) {
-            if (value.query == trendWords[i]){
-                graphData.push(value.date)
-        }}
-        graphData.sort(function (a, b) {
-            a = a.split('/');
-            b = b.split('/');
-            return a[2] - b[2] || a[1] - b[1] || a[0] - b[0];
-        });
-        let freqData = []
-        console.log(graphData)
-        for (const num of graphData) {
-            freqData[num] = freqData[num] ? freqData[num] + 1 : 1;
-          }
-        let finalData = []
-
-        for (var [key,value] of Object.entries(freqData)){
-            finalData.push({
-                date: key,
-                freq: value
-            });
+    for (let i = 0; i < trendWords.length; i++) {
+      let graphData = [];
+      for (var [key, value] of Object.entries(data.data)) {
+        if (value.query == trendWords[i]) {
+          graphData.push(value.date);
         }
-        // console.log(finalData)
+      }
+      graphData.sort(function (a, b) {
+        a = a.split("/");
+        b = b.split("/");
+        return a[2] - b[2] || a[1] - b[1] || a[0] - b[0];
+      });
+      let freqData = [];
+      console.log(graphData);
+      for (const num of graphData) {
+        freqData[num] = freqData[num] ? freqData[num] + 1 : 1;
+      }
+      let finalData = [];
 
-        Graphs[i] = <TrendsLineGraph data={finalData} word={trendWords[i]} />;
+      for (var [key, value] of Object.entries(freqData)) {
+        finalData.push({
+          date: key,
+          freq: value,
+        });
+      }
+      // console.log(finalData)
+
+      Graphs[i] = <TrendsLineGraph data={finalData} word={trendWords[i]} />;
     }
-  console.log(dataTrend)
-}
-  
+    console.log(dataTrend);
+  }
+
   if (trends.status === "failed") {
     Graphs[0] = <p>This Failed</p>;
   }
 
-  const trendsMenu = dropDownMenu(trendWords, "Trends")
-
+  const trendsMenu = dropDownMenu(trendWords, "Trends");
 
   return (
     <>
-    <div>
-      <form onSubmit={onUpdateSubredditNames}>
-        <label>
-          Enter subreddit name:
-          <input
-            type="text"
-            value={subreddit}
-            onChange={(e) => setSubreddit(e.target.value)}
-          />
-        </label>
-        <input type="submit" />
-      </form>
-      
-      <form onSubmit={onUpdateTrendWords}>
-        <label>
-          Enter trend name:
-          <input
-            type="text"
-            value={trendKeyword}
-            onChange={(e) =>{
-                setTrendKeyword(e.target.value)
-                
-            }
-        }
-          />
-        </label>
-        <input type="submit" />
-      </form>
-
       <div>
-      <form onSubmit={handleClickForTrends}>  
-      {trendsMenu}
-      <input type="submit" />
-      </form>
-      <form onSubmit={handleClickForSubreddits}>  
-      {dropDownMenu(subredditNames, "Subreddits")}
-      <input type="submit" />
-      </form>
-    </div>
-    <div>
-      {Graphs[0]}
-      </div>
+        <form onSubmit={onUpdateSubredditNames}>
+          <label>
+            Enter subreddit name:
+            <input
+              type="text"
+              value={subreddit}
+              onChange={(e) => setSubreddit(e.target.value)}
+            />
+          </label>
+          <input type="submit" />
+        </form>
 
+        <form onSubmit={onUpdateTrendWords}>
+          <label>
+            Enter trend name:
+            <input
+              type="text"
+              value={trendKeyword}
+              onChange={(e) => {
+                setTrendKeyword(e.target.value);
+              }}
+            />
+          </label>
+          <input type="submit" />
+        </form>
+
+        <div>
+          <form onSubmit={handleClickForTrends}>
+            {trendsMenu}
+            <input type="submit" />
+          </form>
+          <form onSubmit={handleClickForSubreddits}>
+            {dropDownMenu(subredditNames, "Subreddits")}
+            <input type="submit" />
+          </form>
+        </div>
+        <div>{Graphs[0]}</div>
       </div>
     </>
   );
-    
 }
-
-
 
 export default Trends;
