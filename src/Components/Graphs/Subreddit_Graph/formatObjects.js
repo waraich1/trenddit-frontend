325d2c1a03d7af9a19ac39b8e192d6a97e58b9c1
MahiFaiyaz <68677484+MahiFaiyaz@users.noreply.github.com>
2023-01-27 05:48:09 -0500

    Updated formatData method to clip long author name

--- src/Components/Graphs/Subreddit_Graph/formatObjects.js
+++ src/Components/Graphs/Subreddit_Graph/formatObjects.js
@@ -1,23 +1,27 @@
 
 const formatData = (data, dataKey, dataValue) => {
     let result = []
-    console.log(dataKey)
     const ignoredAuthors = ['AutoModerator', '[deleted]']
     const ignoredWords = ['t', 'moderators', 'r', 'subreddit', 'comment', 'moderation', 'bot', 'submission']
 
     const dataArr = Object.entries(data)
     for (const [key, value] of dataArr) {
         if (dataKey === "author" && ignoredAuthors.includes(key)) {
-            console.log(key)
             continue;
         }
         if (dataKey === "text" && ignoredWords.includes(key)) {
-            console.log(key)
             continue;
         }
         var obj = {}
         obj[dataKey] = key 
         obj[dataValue] = value
+        if (dataKey === "author") {
+            obj['label'] = key
+            if (key.length > 10) {
+                console.log(key.length)
+                obj['label'] = key.slice(0,8) + ".."
+            }
+        }
         result.push(obj)
     }
     result.sort((a,b) => b.comments - a.comments)
