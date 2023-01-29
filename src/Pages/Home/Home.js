c1b41c7cdb8b13ee322fd724d9cbd9ef7d1173ba
MahiFaiyaz <68677484+MahiFaiyaz@users.noreply.github.com>
2023-01-28 19:48:15 -0500

    added google login

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import { Container, Button, Input, Grid, Card, Dimmer, Loader, Statistic, CardContent } from 'semantic-ui-react'
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
+import jwt_decode from "jwt-decode"
+import { useNavigate } from 'react-router';
 
 function Home() {
-    return ( <div><h1>Home</h1></div>)
-}
 
+    const navigate = useNavigate()
+
+    const CredentialResponse = (response) => {
+        const token = response.credential
+        const decoded_token = jwt_decode(token)
+        const email = decoded_token.email
+        navigate("/subreddit")
+    }
+
+    return (
+    <>
+    <Container>
+      <Grid textAlign="center" columns={3} divided padded='vertically'>
+        <Grid.Row>
+          <Grid.Column stretched>
+            <GoogleOAuthProvider clientId="xxx">
+                <GoogleLogin
+                    onSuccess={CredentialResponse}
+                    onError={() => {console.log('Login Failed')}}/>
+            </GoogleOAuthProvider>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Container>
+    </>
+  );
+}
 
 export default Home;
